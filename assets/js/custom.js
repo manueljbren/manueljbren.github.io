084005181fb3951be2623c842b7c30c832643c3f
manueljbren <mjbf.iuris@icloud.com>
2025-08-18 12:02:40 +0200

    actualización

--- assets/js/custom.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const langDropdown = document.querySelector("#navbar-main .dropdown-menu");
-  if (langDropdown) {
-    document.body.appendChild(langDropdown);
-    langDropdown.style.position = "absolute";
-    langDropdown.style.zIndex = "3000";
-  }
-});
